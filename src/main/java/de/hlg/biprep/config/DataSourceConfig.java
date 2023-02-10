e010244cbdc6361b9d0a81812032aa8c690a4bd8
Raggazzi <eli_reza@hotmail.de>
2023-02-10 13:28:10 +0100

    Basic Application Context

---
+++ src/main/java/de/hlg/biprep/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package de.hlg.biprep.config;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("postgres");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db.changelog.xml");
+        return liquibase;
+    }
+}
---
+++ src/test/java/de.hlg.biprep/BookLibraryApplicationTest.java
@@ -0,0 +1,13 @@
+package de.hlg.biprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
+
